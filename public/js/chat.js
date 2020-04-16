01376c7685753391428dd87831e1715929dfbb0b
George Gadelha <george.gadelha@edu.unifor.br>
2020-04-16 19:05:36 -0300

    Adding messages to the screen with time included

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,9 +5,28 @@ const $messageForm = document.querySelector('#message-form');
 const $messageFormInput = document.querySelector('input');
 const $messageFormButton = document.querySelector('button');
 const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
 
-socket.on('message', (data) => {
-    console.log(data)
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
+
+socket.on('message', (message) => {
+    console.log(message);
+    const messageHTML = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    });
+    $messages.insertAdjacentHTML("beforeend", messageHTML);
+});
+
+socket.on('locationMessage', (message) => {
+    console.log(message);
+    const locationHTML = Mustache.render(locationMessageTemplate, {
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    });
+    $messages.insertAdjacentHTML("beforeend", locationHTML);
 });
 
 $messageForm.addEventListener('submit', (e) => {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express();
 const server = http.createServer(app);
@@ -16,25 +17,25 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket Connection')
 
-    socket.emit('message', 'Welcome!')
-    socket.broadcast.emit('message', 'A new user has joined!')
+    socket.emit('message', generateMessage('Welcome!'))
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
-        if(filter.isProfane(message)) {
+        if (filter.isProfane(message)) {
             return callback('Profanity is not allowed!')
         }
-        io.emit('message', message)
+        io.emit('message', generateMessage(message))
         callback(); //ack
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('message', `https://google.com/maps?q=${coords.latitude.toFixed(3)},${coords.longitude.toFixed(3)}`);
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude.toFixed(3)},${coords.longitude.toFixed(3)}`));
         callback();
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', 'User has left!')
+        io.emit('message', generateMessage('User has left!'))
     })
 })
 
